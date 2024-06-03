36ad06077cbcd22b9976e98ed7dfa8f3afc2da09
Nikita Sychev <n.sychev@sh.dunice.net>
2024-06-03 12:06:22 +0300

    Adding multiple examples

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main()
     {
-        System.Console.WriteLine(ArticleTitles.ConvertToArticleCase("the clash of CLANS", "a an the of"));
+        Console.WriteLine(ArticleTitles.ConvertToArticleCase("the clash of CLANS", "a an the of"));
+
+        Console.WriteLine(ArticleTitles.ConvertToArticleCase("В шаге ОТ победы", "в от"));
+        
+        Console.WriteLine(ArticleTitles.ConvertToArticleCase("первый аргумент")); 
     }
 }
